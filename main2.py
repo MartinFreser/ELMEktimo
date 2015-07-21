1c0c69be9c567e1bb182dd569d494340a09fe92e
MartinFreser <martin.freser@gmail.com>
2015-07-21 14:12:17 +0200

    initial commit

---
+++ main2.py
@@ -0,0 +1,8 @@
+import MetaDES.MetaDesParameterFinding
+if __name__ == "__main__":
+    # bla()
+    # main4()
+    # main2LoadResults()
+
+    # MetaDES.HelpersZaOstanek.fullTestProcess()
+    MetaDES.MetaDesParameterFinding.findParameters()
