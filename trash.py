eae9d1d16cfa7db80b2949220c615f77564ea0db
Martin Frešer <martin.freser@gmail.com>
2015-08-12 14:27:36 +0200

    We wrote function that removes header and first two features (ticker and date) from fullTestFeatures.csv in trash.py

--- trash.py
+++ trash.py
@@ -1,6 +1,18 @@
 __author__ = 'Martin'
+import numpy as np
 def maliKos():
     with open("//./Z:/Podatki/Prediction datasets/fullTestFeatures.csv") as f:
-        with open("data/fullTestFeaturesMaliKos.csv", "w") as f2:
-            for i in range(2000):
-                f2.write(f.readline())
+        i = 0
+        f.readline() #dummy read for header
+        with open("//./Z:/Martin Freser/ELMEktimo/data/dataForMeta/ostanek/fullTestFeatures.csv", "w") as f2:
+            for line in f:
+                i+=1
+                content = np.array(line.replace("\"", "").split(",")[2:]).astype(float)
+                content = ",".join(map(str,list(content)))
+                f2.write(content)
+                if(i%100000 == 0): print("%d/%d" %(i, 1000000))
+        print("we wrote %d lines" %i)
+
+
+if __name__ == "__main__":
+    maliKos()
